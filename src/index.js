f1b8b2f7e318cb0628e177fd8f2f6b673ce6a853
Noma-tee <nomatee28@gmail.com>
2024-11-16 11:28:14 +0200

    added apiKey

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "f697a4e486b197d55349o70t340192cc";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric";
+  console.log(apiURL);
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-alert("hye");
